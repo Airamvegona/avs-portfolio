2034cc618026981478ba6f8a3634aab43966a371
Airamvegona <airamvegasuarez37@gmail.com>
2025-01-13 14:35:26 +0100

    no rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,4 +13,20 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
-export default eslintConfig;
+module.exports = {
+  ...eslintConfig,
+  rules: {
+    // Desactivar todas las reglas
+    "no-empty-object-type": "off",
+    "no-unused-vars": "off",
+    "react/no-unescaped-entities": "off",
+    "@typescript-eslint/no-empty-object-type": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/ban-types": "off",
+    "react/prop-types": "off",
+    "react/jsx-uses-react": "off",
+    "react/jsx-uses-vars": "off",
+  },
+};
